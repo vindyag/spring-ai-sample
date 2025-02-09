88591a3b7b5817b9a4fb501ca43740bfb432e6c1
vindyag <vindyag@Vindyas-MacBook-Pro.local>
2025-02-09 13:09:24 +0100

    add inital repo'

---
+++ src/test/java/demo/SpringAiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package vindya.spring_ai_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
